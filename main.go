5c574112bcfe13de38a31a1d911fbc2714a5b6bc
Jixkls <cloudproducers@gmail.com>
2026-02-11 23:09:44 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type User struct {
+    ID        int    `json:"id"`
+    FirstName string `json:"first_name"`
+}
+
+type Chat struct {
+    ID       int    `json:"id"`
+    ChatType string `json:"type"`
+}
+
+type Message struct {
+    MessageID int    `json:"message_id"`
+    From      User   `json:"from"`
+    Chat      Chat   `json:"chat"`
+    Text      string `json:"text"`
+}
+
+//struct maior
+type Update struct {
+    UpdateID int     `json:"update_id"`
+    Message  Message `json:"message"`
+}
+
+type SendMessage struct {
+	ChatID int    `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+func sendReply(ChatID int, Text string) {
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	url := "https://api.telegram.org/bot" + token + "/sendMessage"
+
+	msgBody := SendMessage{
+		ChatID: ChatID,
+		Text:   Text,
+	}
+
+	jsonData, err := json.Marshal(msgBody)
+	if err != nil {
+		fmt.Println("Error marshaling reply:", err)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error sending reply:", err)
+	}
+	defer resp.Body.Close()
+}
+
+func webhookHandler(wri http.ResponseWriter, req *http.Request){
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("Error reading request body:", err)
+		return
+	}
+	defer req.Body.Close()
+
+	var update Update
+	err = json.Unmarshal(body, &update)
+	if err != nil {
+		fmt.Println("Error unmarshaling update:", err)
+		return
+	}
+
+	userText := update.Message.Text
+	chatID := update.Message.Chat.ID
+
+	fmt.Printf("Received message: %s from chat ID: %d\n", userText, chatID)
+
+	sendReply(chatID, "You said: "+userText)
+
+}
+
+func main() {
+	godotenv.Load()
+
+	http.HandleFunc("/webhook", webhookHandler)
+
+	fmt.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
