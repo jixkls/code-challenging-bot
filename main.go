a77156fa8b716774dccfe23fa26e4a40e40cd4ef
Jixkls <cloudproducers@gmail.com>
2026-02-14 20:17:56 -0300

    Update fallback for port 8080

--- main.go
+++ main.go
@@ -90,7 +90,12 @@ func main() {
 
 	http.HandleFunc("/webhook", webhookHandler)
 
-	fmt.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 
+	fmt.Println("Starting server on :8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" //fallback para porta
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
